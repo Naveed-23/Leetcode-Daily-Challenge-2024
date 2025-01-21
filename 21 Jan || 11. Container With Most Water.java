476d2db91081ddb33992603c99489d1a73e25bf6
Naveed-23 <naveedbh23@gmail.com>
2025-01-21 08:01:44 +0530

    Solve 11. Container With Most Water

---
+++ 21 Jan || 11. Container With Most Water.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int maxWater = 0;
+        int left = 0;
+        int right = height.length - 1;
+        while(left < right){
+            int width = right - left;
+            int ht = Math.min(height[left], height[right]);
+            int currWater = width * ht;
+            maxWater = Math.max(maxWater, currWater);
+            if(height[left] < height[right]){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        return maxWater;
+    }        
+}
